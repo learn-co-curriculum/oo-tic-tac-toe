0f4e5712d1c375be7f16767a860550123435f4f8
mendelB <mendelblesofsky@gmail.com>
2016-04-27 16:54:58 -0400

    change win_combo test to not be specific abt order of the numbers

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -13,14 +13,14 @@ describe './lib/tic_tac_toe.rb' do
       it 'defines a constant WIN_COMBINATIONS with arrays for each win combination' do
         expect(TicTacToe::WIN_COMBINATIONS.size).to eq(8)
 
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,1,2])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([3,4,5])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([6,7,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,3,6])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([1,4,7])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([2,5,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([0,4,8])
-        expect(TicTacToe::WIN_COMBINATIONS).to include([6,4,2])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,1,2])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([3,4,5])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([6,7,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,3,6])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([1,4,7])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([2,5,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([0,4,8])
+        expect(TicTacToe::WIN_COMBINATIONS).to include_array([6,4,2])
       end
     end
 
